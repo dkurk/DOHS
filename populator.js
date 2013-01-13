703091433059cf2e49e8fc2af75fe3fb11bd8dd2
David Kurkovskiy <brainmonkey95@gmail.com>
2013-01-13 01:36:07 -0500

    played around with some form stuff still have to make this stuff work it's a big mess right now

--- populator.js
+++ populator.js
@@ -3,17 +3,50 @@ function loadPeople() {
 	$(".person").remove();
 	var svgns="http://www.w3.org/2000/svg";
 	for (var i in people) {
-	    if (balls[i][0]==name) continue;
-	    var b = document.createElementNS(svgns, "circle");
-	    b.setAttributeNS(null, 'class', 'person');
-	    b.setAttributeNS(null, 'cx', balls[i][1]);
-	    b.setAttributeNS(null, 'cy', balls[i][2]);
-	    b.setAttributeNS(null, 'r', 20);
-	    b.setAttributeNS(null, 'stroke', 'black');
-	    b.setAttributeNS(null, 'fill', 'green');
+	    if (people[i][0]==name) continue;
+	    var p = document.createElementNS(svgns, "circle");
+	    p.setAttributeNS(null, 'class', 'person');
+	    p.setAttributeNS(null, 'cx', people[i][1]);
+	    p.setAttributeNS(null, 'cy', people[i][2]);
+	    p.setAttributeNS(null, 'r', 20);
+	    p.setAttributeNS(null, 'stroke', 'black');
+	    p.setAttributeNS(null, 'fill', 'green');
+	    switch(people[i][3]) { }
 	}
     });
 }
 
+var person = function(id, first, last, pd1, pd2, pd3, pd4, pd5, pd6, pd7, pd8, pd9, pd10) {
+    this.id = id;
+    this.first = first;
+    this.last = last;
+    this.pd1 = pd1;
+    this.pd2 = pd2;
+    this.pd3 = pd3;
+    this.pd4 = pd4;
+    this.pd5 = pd5;
+    this.pd6 = pd6;
+    this.pd7 = pd7;
+    this.pd8 = pd8;
+    this.pd9 = pd9;
+    this.pd10 = pd10;
+}
+
+function login(e) {
+    if (e.charCode==13) {
+	id = $(this).val();
+	$.getJSON('/register', {name:name}, function(d) {
+	    console.log(d);
+	    if (d==false) {
+		break;
+	    }
+	    else {
+		id = d[0];
+	    }
+	});
+    }
+}
+
 function makePerson() {
-    me = #('<
+    me = #();
+}
