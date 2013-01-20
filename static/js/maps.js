0788590c2e2a1c71e86f74dc4a5a966c55eef324
David Kurkovskiy <brainmonkey95@gmail.com>
2013-01-20 08:32:13 -0500

    Adding the css and js folders to static

---
+++ static/js/maps.js
@@ -0,0 +1,73 @@
+/*func loadMaps()
+**@params: none
+**This function calls the serve method "getPeople" and pulls out a list of people dictionaries, consisting of student users and pre-added teachers from the database. For each person, it creates a new <circle> element that will be added to the svg that corresponds with the floor this user/teacher should be on during first period. A later update method will respond to changes in floor
+*/
+
+var loadMaps = function() {
+
+    $.getJSON("/getPeople", function(people) {
+	$(".person").remove();
+	var svgns = "http://www.w3.org/2000/svg";
+	for (var i in people) {
+	    var newCircle = document.createElementNS(svgns, "circle");
+	    newCircle.setAttributeNS(null, 'class', 'person');
+	    newCircle.setAttributeNS(null, 'id', people['id']);
+	    newCircle.setAttributeNS(null, 'cx', Math.floor(Math.random()*500));
+	    newCircle.setAttributeNS(null, 'cy', Math.floor(Math.random()*250));
+	    newCircle.setAttributeNS(null, 'r', 10);
+	    newCircle.setAttributeNS(null, 'stroke', 'black');
+	    newCircle.setAttributeNS(null, 'fill', 'green');
+	    switch (people['rooms'][0] % 100) {
+	    case 1:
+		$("#svg-one").append(newCircle);
+		break;
+	    case 2:
+		$("#svg-two").append(newCircle);
+		break;
+	    case 3:
+		$("#svg-three").append(newCircle);
+		break;
+	    case 4:
+		$("#svg-four").append(newCircle);
+		break;
+	    case 5:
+		$("#svg-five").append(newCircle);
+		break;
+	    case 6:
+		$("#svg-six").append(newCircle);
+		break;
+	    case 7:
+		$("#svg-seven").append(newCircle);
+		break;
+	    case 8:
+		$("#svg-eight").append(newCircle);
+		break;
+	    case 9:
+		$("#svg-nine").append(newCircle);
+		break;
+	    case 10:
+		$("#svg-ten").append(newCircle);
+		break;
+	    }
+	}
+    });
+}
+
+/*func getProfile()
+**@params: none
+**This function, assigned on click to each bubble representing a student or teacher on the 10 svg maps takes the ID number/keyword of the clicked user and calls the "getProfile" server-side method to return the object the bubble represents. The profile information of this user/teacher will then be printed on a div at the bottom of the page. 
+*/
+var getProfile = function() {
+
+    var myID = $(this).attr("id");
+    $.getJSON("/getProfile", {id:myID}, function(person) {
+	$("#profile").text(JSON.stringify(person));
+    });
+
+}
+
+
+$(document).ready(function() {
+    loadMaps();
+    $(".person").click(getProfile);
+});
