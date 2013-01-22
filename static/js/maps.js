82263fdb315ab89eaf406657c53a1a457cceefec
HelenNie <artigkaninhn@gmail.com>
2013-01-22 03:14:07 -0500

    close button working

--- static/js/maps.js
+++ static/js/maps.js
@@ -174,7 +174,7 @@ var setPeriod = function() {
 }
 
 var zoom = function() {
-    TINY.box.show({html: "<style> #svg-zoom {border-style:solid;} </style> <svg id='svg-zoom' xmlns='http://www.w3.org/2000/svg' version='1.1' width='400' height='400'></svg>", width:450, height:450});
+    TINY.box.show({html: "<img src='static/jpg/close2.png' Onclick='TINY.box.hide()'>Close</img><style> #svg-zoom {border-style:solid;} </style> <svg id='svg-zoom' xmlns='http://www.w3.org/2000/svg' version='1.1' width='400' height='400'></svg>", width:450, height:450});
 }
  
 $(document).ready(function() {
