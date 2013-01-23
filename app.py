992e458e2b12b7011441f443ee6408e4fe0b05c4
David Kurkovskiy <brainmonkey95@gmail.com>
2013-01-22 19:36:12 -0500

    Added the return to maps button in update and added a link between the button in maps and the update page

--- app.py
+++ app.py
@@ -98,7 +98,10 @@ Last edited: 1/21/13 at 12:24 by Helen Nie
 def update():
     if request.method=='GET':
         return render_template("update.html", ID=session['ID'])
-
+    else:
+        button = request.form['button']
+        if button == "Go back to the maps!":
+            return redirect(url_for('maps'))
 
 
 """
@@ -117,6 +120,8 @@ def maps():
         button = request.form['button']
         if button == "Logout":
             return redirect(url_for('logout'))
+        elif button == "View/Update Your Profile":
+            return redirect(url_for('update'))
 
 
 
