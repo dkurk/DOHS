a825cc42ce1cfb5574926f10935f5f8c9df9a26f
HelenNie <artigkaninhn@gmail.com>
2013-01-22 20:12:13 -0500

    twilio methods done in app.py

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ Last edited: 1/21/13 at 12:24 by Helen Nie
 """
 
 global floor, period
-floor = period = 2
+floor = period = 1
 global IDs 
-IDs = ["8751"]
+IDs = ["1111"]
 
 
 
@@ -57,6 +57,7 @@ def login():
         ID = request.form['ID']
         button = request.form['button']
         if button == 'Login':
+            #need ifUserExists(ID) method from db.py, is doesnt exist, direct to account page
             if db.userExists(ID):
                 session['ID'] = ID
                 if 'redirectpage' in session:
@@ -81,10 +82,6 @@ Last edited: 1/21/13 at 12:24 by Helen Nie
 def account():
     if request.method=='GET':
         return render_template("account.html")
-    else:
-        button = request.form['button']
-        if button == 'Account created. Click to log in!':
-            return redirect(url_for('login'))
 
 
 """
@@ -98,10 +95,7 @@ Last edited: 1/21/13 at 12:24 by Helen Nie
 def update():
     if request.method=='GET':
         return render_template("update.html", ID=session['ID'])
-    else:
-        button = request.form['button']
-        if button == "Go back to the maps!":
-            return redirect(url_for('maps'))
+
 
 
 """
@@ -120,8 +114,6 @@ def maps():
         button = request.form['button']
         if button == "Logout":
             return redirect(url_for('logout'))
-        elif button == "View/Update Your Profile":
-            return redirect(url_for('update'))
 
 
 
@@ -301,10 +293,8 @@ Last edited: 1/21/13 at 12:24 by Helen Nie
 @app.route("/getTeacherLoc")
 def getTeacherLoc():
     global period
-    #last = request.args.get('last', '')
-    last = "zamansky"
-    period = "6"
-    
+    last = request.args.get('last', '')
+        
     value = db.getTeacherLoc(last, period)
     return json.dumps(value)
 
